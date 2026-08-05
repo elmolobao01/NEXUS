521faa016c6212f2099c108eed20b80aabf0822d
elmolobao01 <elmolobao@gmail.com>
2026-08-05 20:57:38 -0300

    Add files via upload

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,7 @@
+import { NexusShell } from "../../components/layout/nexus-shell";
+
+export const metadata = { title: "Administração NEXUS" };
+
+export default function AdminPage() {
+  return <NexusShell />;
+}
