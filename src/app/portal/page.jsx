b53b12ffdae2258f128417a756e3965026febb0b
elmolobao01 <elmolobao@gmail.com>
2026-08-05 20:59:34 -0300

    Add files via upload

---
+++ src/app/portal/page.jsx
@@ -0,0 +1,7 @@
+import { ClientPortalShell } from "../../components/client/client-portal-shell";
+
+export const metadata = { title: "Portal do Cliente | NEXUS" };
+
+export default function PortalPage() {
+  return <ClientPortalShell />;
+}
