7862cdf474e675e07d8435ce0782fa20f88ab16f
elmolobao01 <elmolobao@gmail.com>
2026-09-07 12:43:10 -0300

    Add files via upload

--- src/core/assinaturas/central-cliente.js
+++ src/core/assinaturas/central-cliente.js
@@ -1,6 +1,6 @@
-import { MODULOS_NEXUS } from "../catalogos/modulos";
-import { FACILITIES_NEXUS } from "../catalogos/facilities";
-import { PRODUTOS_NEXUS } from "../catalogos/produtos";
+import { MODULOS_PLENIUM } from "../catalogos/modulos";
+import { FACILITIES_PLENIUM } from "../catalogos/facilities";
+import { PRODUTOS_PLENIUM } from "../catalogos/produtos";
 
 export function resumoCentral(organizacao) {
   const assinatura = organizacao?.assinatura || {};
@@ -12,9 +12,9 @@ export function resumoCentral(organizacao) {
     renovacaoEm: assinatura.renovacaoEm || null,
     valorMensal: assinatura.valorMensal || 0,
     economiaMensal: assinatura.economiaMensal || 0,
-    produtos: (assinatura.produtos || []).map((id) => PRODUTOS_NEXUS[id]).filter(Boolean),
-    modulos: (assinatura.entitlements?.modulos || []).map((id) => MODULOS_NEXUS[id]).filter(Boolean),
-    facilities: (assinatura.entitlements?.facilities || []).map((id) => Object.values(FACILITIES_NEXUS).find((item) => item.id === id)).filter(Boolean),
+    produtos: (assinatura.produtos || []).map((id) => PRODUTOS_PLENIUM[id]).filter(Boolean),
+    modulos: (assinatura.entitlements?.modulos || []).map((id) => MODULOS_PLENIUM[id]).filter(Boolean),
+    facilities: (assinatura.entitlements?.facilities || []).map((id) => Object.values(FACILITIES_PLENIUM).find((item) => item.id === id)).filter(Boolean),
     limites: assinatura.entitlements?.limites || {},
   };
 }
